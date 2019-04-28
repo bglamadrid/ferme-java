637ab44f35b9a573b44718248b89b34d842b28b7
Benjamin <got12g@gmail.com>
2019-04-28 17:07:42 -0400

    reimplementa, mejora el servicio de rubros para obtener entidades en base a filtros creados dinamicamente con QueryDSL

--- ferme-web/src/main/java/cl/duoc/alumnos/ferme/services/IRubrosService.java
+++ ferme-web/src/main/java/cl/duoc/alumnos/ferme/services/IRubrosService.java
@@ -2,7 +2,9 @@ package cl.duoc.alumnos.ferme.services;
 
 import cl.duoc.alumnos.ferme.domain.entities.Rubro;
 import cl.duoc.alumnos.ferme.dto.RubroDTO;
+import com.querydsl.core.types.Predicate;
 import java.util.Collection;
+import java.util.Map;
 
 /**
  *
@@ -13,6 +15,14 @@ public interface IRubrosService {
     public Rubro rubroDTOToEntity(RubroDTO dto);
     public RubroDTO rubroEntityToDTO(Rubro entity);
     
-    public Collection<RubroDTO> getRubros();
+    /**
+     * Obtiene todos los rubros a partir de un filtro determinado y los 
+     * transforma a DTO.
+     * @param condicion El objeto Predicate con los filtros. Puede ser null.
+     * @return Una colección de objetos DTO.
+     */
+    public Collection<RubroDTO> getRubros(Predicate condicion);
+    
+    public Predicate queryParamsMapToFilteringPredicate(Map<String,String> nombre);
     
 }
--- ferme-web/src/main/java/cl/duoc/alumnos/ferme/services/imp/RubrosService.java
+++ ferme-web/src/main/java/cl/duoc/alumnos/ferme/services/imp/RubrosService.java
@@ -1,14 +1,20 @@
 package cl.duoc.alumnos.ferme.services.imp;
 
+import cl.duoc.alumnos.ferme.domain.entities.QRubro;
 import cl.duoc.alumnos.ferme.domain.entities.Rubro;
-import cl.duoc.alumnos.ferme.domain.repositories.RubrosRepository;
 import cl.duoc.alumnos.ferme.dto.RubroDTO;
 import cl.duoc.alumnos.ferme.services.IRubrosService;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import javax.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import cl.duoc.alumnos.ferme.domain.repositories.IRubrosRepository;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
@@ -17,11 +23,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class RubrosService implements IRubrosService {
     
-    @Autowired RubrosRepository rubroRepo;
+    @Autowired IRubrosRepository rubroRepo;
+    @Autowired EntityManager em;
 
     @Override
     public Rubro rubroDTOToEntity(RubroDTO dto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
@@ -35,15 +42,44 @@ public class RubrosService implements IRubrosService {
     }
 
     @Override
-    public Collection<RubroDTO> getRubros() {
+    public Collection<RubroDTO> getRubros(Predicate condicion) {
         
         List<RubroDTO> pagina = new ArrayList<>();
-        this.rubroRepo.findAll().forEach((entity) -> {
+        
+        Iterable<Rubro> rubroIterable;
+        if (condicion == null) {
+            rubroIterable = rubroRepo.findAll();
+        } else {
+            rubroIterable = rubroRepo.findAll(condicion);
+        }
+        
+        rubroIterable.forEach((entity) -> {
             RubroDTO dto = this.rubroEntityToDTO(entity);
             pagina.add(dto);
         });
         
         return pagina;
+    }
+    
+    @Override
+    public Predicate queryParamsMapToFilteringPredicate(Map<String,String> filtros){
+        
+        QRubro qRubro = QRubro.rubro;
+        BooleanBuilder bb = new BooleanBuilder();
+        for (String key : filtros.keySet()) {
+            String value = filtros.get(key);
+            switch (key) {
+                case "id":
+                    bb = bb.and(qRubro.idRubro.eq(Integer.valueOf(value))); //TODO validar que el valor sea entero
+                    return bb; //match por id es único
+                case "descripcion":
+                    bb = bb.and(qRubro.descripcion.like(value));
+                    break;
+                default: break;
+            }
+        }
+        
+        return bb;
     }
     
 }

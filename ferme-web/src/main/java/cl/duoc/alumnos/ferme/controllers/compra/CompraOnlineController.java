38036280e0ac467b95b5ed8355be209cfd7aadc9
Benjamin <got12g@gmail.com>
2019-05-01 22:31:49 -0400

    cambio menor

--- ferme-web/src/main/java/cl/duoc/alumnos/ferme/controllers/compra/CompraOnlineController.java
+++ ferme-web/src/main/java/cl/duoc/alumnos/ferme/controllers/compra/CompraOnlineController.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package cl.duoc.alumnos.ferme.controllers;
+package cl.duoc.alumnos.ferme.controllers.compra;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;

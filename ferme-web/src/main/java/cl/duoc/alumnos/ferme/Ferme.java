f92d543a1ec7a07c5a78460583cbfdfc67bde317
Benjamin <got12g@gmail.com>
2019-05-04 21:32:45 -0400

    añade documentacion de las propiedades al proyecto y un mensaje de inicio exitoso propio de la aplicación

--- ferme-web/src/main/java/cl/duoc/alumnos/ferme/Ferme.java
+++ ferme-web/src/main/java/cl/duoc/alumnos/ferme/Ferme.java
@@ -1,5 +1,8 @@
 package cl.duoc.alumnos.ferme;
 
+import cl.duoc.alumnos.ferme.controllers.ComunController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,9 +16,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("cl.duoc.alumnos.ferme.*")
 @EntityScan("cl.duoc.alumnos.ferme.domain.entities")
 public class Ferme {
+    private final static Logger LOG = LoggerFactory.getLogger(ComunController.class);
         
     public static void main(String[] args) {
         SpringApplication.run(FermeConfig.class, args);
+        LOG.info("Aplicación FERME cargada e iniciada exitosamente.");
     }
     
 }

ac09ae120d7e352f394db70ba9678d7b872aa8c0
Benjamin <got12g@gmail.com>
2019-05-19 12:23:18 -0400

    [personas] añade servicio con métodos para listar y filtrar

---
+++ ferme-web/src/main/java/cl/duoc/alumnos/ferme/services/PersonasService.java
@@ -0,0 +1,85 @@
+package cl.duoc.alumnos.ferme.services;
+
+import cl.duoc.alumnos.ferme.domain.entities.Cliente;
+import cl.duoc.alumnos.ferme.domain.entities.Persona;
+import cl.duoc.alumnos.ferme.domain.entities.QPersona;
+import cl.duoc.alumnos.ferme.domain.repositories.IPersonasRepository;
+import cl.duoc.alumnos.ferme.dto.ClienteDTO;
+import cl.duoc.alumnos.ferme.dto.PersonaDTO;
+import cl.duoc.alumnos.ferme.services.interfaces.IPersonasService;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+/**
+ *
+ * @author Benjamin Guillermo <got12g at gmail.com>
+ */
+public class PersonasService implements IPersonasService {
+    private final static Logger LOG = LoggerFactory.getLogger(PersonasService.class);
+    
+    @Autowired IPersonasRepository personaRepo;
+
+    @Override
+    public Collection<PersonaDTO> getPersonas(int pageSize, int pageIndex, Predicate condicion) {
+        Pageable pgbl = PageRequest.of(pageIndex, pageSize);
+        
+        List<PersonaDTO> pagina = new ArrayList<>();
+        Iterable<Persona> pesonas;
+        
+        if (condicion == null) {
+            pesonas = personaRepo.findAll(pgbl);
+        } else {
+            pesonas = personaRepo.findAll(condicion, pgbl);
+        }
+        
+        pesonas.forEach((entity) -> {
+            PersonaDTO dto = entity.toDTO();
+            pagina.add(dto);
+        });
+        
+        return pagina;
+    }
+
+    @Override
+    public Predicate queryParamsMapToPersonasFilteringPredicate(Map<String, String> queryParamsMap) {
+        
+        QPersona qPersona = QPersona.persona;
+        BooleanBuilder bb = new BooleanBuilder();
+        for (String paramName : queryParamsMap.keySet()) {
+            String paramValue = queryParamsMap.get(paramName);
+            LOG.debug(paramName+"="+paramValue);
+            try {
+                Integer parsedValueI;
+                switch (paramName) {
+                    case "id":
+                        parsedValueI = Integer.valueOf(paramValue);
+                        bb.and(qPersona.id.eq(parsedValueI));
+                        return bb; //match por id es único
+                    case "nombre":
+                        paramValue = "%" + paramValue.toUpperCase() + "%";
+                        bb.and(qPersona.nombreCompleto.likeIgnoreCase(paramValue));
+                        break;
+                    case "rut":
+                        paramValue = "%" + paramValue.toUpperCase() + "%";
+                        bb.and(qPersona.rut.likeIgnoreCase(paramValue));
+                        break;
+                    default: break;
+                }
+            } catch (NumberFormatException exc) {
+                LOG.error("No se pudo traducir el parámetro '" + paramName + "' a un número (su valor era '" + paramValue + "').", exc);
+            }
+        }
+        
+        return bb;
+    }
+    
+}
---
+++ ferme-web/src/main/java/cl/duoc/alumnos/ferme/services/interfaces/IPersonasService.java
@@ -0,0 +1,32 @@
+package cl.duoc.alumnos.ferme.services.interfaces;
+
+import cl.duoc.alumnos.ferme.dto.PersonaDTO;
+import com.querydsl.core.types.Predicate;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ *
+ * @author Benjamin Guillermo La Madrid <got12g at gmail.com>
+ */
+public interface IPersonasService {
+    
+    /**
+     * Obtiene una página (colección) de personas, con un tamaño determinado 
+     * (pudiendo filtrarlos) y los transforma a objetos DTO.
+     * @param pageSize El número de resultados que la página mostrará.
+     * @param pageIndex El número de página (la primera es 0).
+     * @param condicion El objeto Predicate con los filtros a aplicar. Nulable.
+     * @return Una colección de objetos DTO.
+     */
+    public Collection<PersonaDTO> getPersonas(int pageSize, int pageIndex, Predicate condicion);
+    
+    /**
+     * Genera una o varias condiciones como un objeto Predicate para filtrar 
+     * personas, tomando un Map de parámetros como base.
+     * @param queryParamsMap Un Map de parámetros (presumiblemente proveído por 
+     * un query string).
+     * @return Un objeto Predicate representando un conjunto de filtros.
+     */
+    public Predicate queryParamsMapToPersonasFilteringPredicate(Map<String,String> queryParamsMap);
+}

114dc7bc8a3ac5e84f344a97413f40baeef50458
Benjamin La Madrid <got12g@gmail.com>
2019-06-24 18:58:34 -0400

    configura jackson para ignorar campos y propiedades desconocidos en general

--- ferme-rest-api/src/main/java/cl/duoc/alumnos/ferme/FermeConfig.java
+++ ferme-rest-api/src/main/java/cl/duoc/alumnos/ferme/FermeConfig.java
@@ -1,6 +1,7 @@
 package cl.duoc.alumnos.ferme;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import java.util.Map;
@@ -86,6 +87,7 @@ public class FermeConfig implements WebMvcConfigurer {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
     }
     
